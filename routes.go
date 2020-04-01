7b685826bf37f963b77651ad05901180d7991dc4
Nuno <nm.morais@campus.fct.unl.pt>
2020-04-01 12:22:18 +0100

    fix routes

--- routes.go
+++ routes.go
@@ -2,31 +2,28 @@ package main
 
 import (
 	"github.com/NOVAPokemon/utils"
+	"github.com/NOVAPokemon/utils/api"
 )
-
 
 const GET = "GET"
 const POST = "POST"
 
 const GetShopItems = "GET_ITEMS"
-const GetShopItemsPath = "/shop/items/"
-
 const BuyItem = "BUY_ITEM"
-const BuyItemPath = "/shop/items/buy/{itemName}"
 
 var routes = utils.Routes{
 
 	utils.Route{
 		Name:        GetShopItems,
 		Method:      GET,
-		Pattern:     GetShopItemsPath,
+		Pattern:     api.GetShopItemsPath,
 		HandlerFunc: HandleGetItems,
 	},
 
 	utils.Route{
 		Name:        BuyItem,
 		Method:      GET,
-		Pattern:     BuyItemPath,
+		Pattern:     api.BuyItemPath,
 		HandlerFunc: HandleBuyItem,
 	},
 }
